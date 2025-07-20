6fc73860c1dfd6b09da1c449cbece5e1082404b1
aaronchasan <57459684+aaronchasan@users.noreply.github.com>
2025-07-20 11:11:47 -0700

    Delete printouts.sh

--- printouts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🖨️  Creating card sheet printouts..."
-source venv/bin/activate
-python make_card_sheets.py
-echo ""
-echo "🎉 Printouts created! Check for card_sheet_*.pdf files." 
